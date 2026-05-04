f52e035287b6244b98d723741a5a4d8544985cf5
garangmalek <garangmalekgom@gmail.com>
2026-05-04 18:34:36 +0300

    refactor the entire codebase

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '841445ca-79f0-4055-9957-f7cd8eda3bb9';
+export const MODAL_CLOSE_SEC = 2.5;
